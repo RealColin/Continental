0a070cfea1fa6812852cb34f78c65813ac5688e9
Werner, Colin <crwgond@gmail.com>
2025-08-21 20:52:05 -0500

    Initial commit

---
+++ common/src/main/java/realcolin/continental/Constants.java
@@ -0,0 +1,11 @@
+package realcolin.continental;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "continental";
+	public static final String MOD_NAME = "Continental";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
